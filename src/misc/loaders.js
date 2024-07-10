6fe6225570641bafc64d0faf83f7a28182dbec9c
sayantan <123sayantandas@gmail.com>
2024-07-10 14:15:23 +0530

    URL changing

--- src/misc/loaders.js
+++ src/misc/loaders.js
@@ -2,12 +2,16 @@ import { defer } from "react-router-dom";
 
 async function loadDashboardData(adminJwt) {
     try {
-        const response = await fetch("http://localhost:5000/admin/dashboard", {
+        // const response = await fetch("http://localhost:5000/admin/dashboard", {
+        const response = await fetch(
+          "https://adopet-backend.onrender.com/admin/dashboard",
+          {
             headers: {
-                "Content-Type": "application/json",
-                'authorization': `Bearer ${adminJwt}`,
+              "Content-Type": "application/json",
+              authorization: `Bearer ${adminJwt}`,
             },
-        });
+          }
+        );
         const data = await response.json();
         if (!response.ok) {
             throw new Error(data || "Failed to fetch");
@@ -21,12 +25,16 @@ async function loadDashboardData(adminJwt) {
 
 async function loadAlladoptPosts(adminJwt) {
     try {
-        const response = await fetch("http://localhost:5000/admin/allAdoptPost", {
+        // const response = await fetch("http://localhost:5000/admin/allAdoptPost", {
+        const response = await fetch(
+          "https://adopet-backend.onrender.com/admin/allAdoptPost",
+          {
             headers: {
-                "Content-Type": "application/json",
-                'authorization': `Bearer ${adminJwt}`,
+              "Content-Type": "application/json",
+              authorization: `Bearer ${adminJwt}`,
             },
-        });
+          }
+        );
         const data = await response.json();
         if (!response.ok) {
             throw new Error(data || "Failed to fetch");
@@ -40,12 +48,16 @@ async function loadAlladoptPosts(adminJwt) {
 
 async function loadAllrescuePosts(adminJwt) {
     try {
-        const response = await fetch("http://localhost:5000/admin/allRescuePost", {
+        // const response = await fetch("http://localhost:5000/admin/allRescuePost", {
+        const response = await fetch(
+          "https://adopet-backend.onrender.com/admin/allRescuePost",
+          {
             headers: {
-                "Content-Type": "application/json",
-                'authorization': `Bearer ${adminJwt}`,
+              "Content-Type": "application/json",
+              authorization: `Bearer ${adminJwt}`,
             },
-        });
+          }
+        );
         const data = await response.json();
         if (!response.ok) {
             throw new Error(data || "Failed to fetch");
@@ -59,12 +71,16 @@ async function loadAllrescuePosts(adminJwt) {
 
 async function loadAllUsers(adminJwt) {
     try {
-        const response = await fetch("http://localhost:5000/admin/allUser", {
+        // const response = await fetch("http://localhost:5000/admin/allUser", {
+        const response = await fetch(
+          "https://adopet-backend.onrender.com/admin/allUser",
+          {
             headers: {
-                "Content-Type": "application/json",
-                'authorization': `Bearer ${adminJwt}`,
+              "Content-Type": "application/json",
+              authorization: `Bearer ${adminJwt}`,
             },
-        });
+          }
+        );
         const data = await response.json();
         if (!response.ok) {
             throw new Error(data || "Failed to fetch");
